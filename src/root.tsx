7389c36573a8936c0afc8bca6cd3cc699e52f7ef
harshmangalam <harshdev8218@gmail.com>
2024-01-07 13:38:46 +0530

    test geolocation

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,8 @@
 import { component$ } from "@builder.io/qwik";
+import { useGeolocation } from "./hooks/use-geolocation";
 
 export default component$(() => {
+  const geolocation = useGeolocation();
   return (
     <>
       <head>
@@ -9,6 +11,9 @@ export default component$(() => {
       </head>
       <body>
         <h1>Qwik Use</h1>
+
+        <h2>Geolocation Hooks</h2>
+        <pre>{JSON.stringify(geolocation, null, 4)}</pre>
       </body>
     </>
   );
